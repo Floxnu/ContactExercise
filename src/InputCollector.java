b8334ad181df070bda0e84bd1f3ce4c074dda645
Yuto <veda.ym0209@gmail.com>
2018-10-24 23:14:35 -0700

    Adding some prompt message and input value.

--- src/InputCollector.java
+++ src/InputCollector.java
@@ -5,8 +5,18 @@ public class InputCollector {
 
     public InputCollector() {
         this.in = new Scanner(System.in);
+
+        System.out.println("Please enter your first name.");
+        String firstName = in.next();
+        System.out.println("Please enter your last name.");
+        String lastName = in.next();
+        System.out.println("Please enter your email address.");
+        String email = in.next();
+        System.out.println("Please enter your phone number.");
+        long phoneNum = in.nextLong();
     }
 
     public String inputForPrompt(String prompt){ return null; }
 
 }
+//add push commit
